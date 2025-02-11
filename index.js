799f02ec6ca69db28ff773946de4f0b6f1be13bf
radha-rani-n <nradharani1998@gmail.com>
2025-02-11 07:03:02 -0500

    add home route api for the page

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
+import "dotenv/config";
 const app = express();
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.use(cors());
 app.use(express.json());
-
-app.listen("PORT", () => {
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
 });
