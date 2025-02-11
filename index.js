cdb4eba784fa014f3c0435d789cf51127cec06e9
radha-rani-n <nradharani1998@gmail.com>
2025-02-10 22:51:49 -0500

    add knexfile.js and create and add data base details in server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 
-app.listen("8080", () => {
-  console.log("Server is listening on port 8080");
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+app.listen("PORT", () => {
+  console.log("Server is listening on port " + PORT);
 });
