1f2ef7307be3a027ce6734b25fbca4d1e52e2c92
radha-rani-n <nradharani1998@gmail.com>
2025-02-11 08:07:32 -0500

    modify .env file to resolve the error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 const app = express();
 
-const { PORT } = process.env;
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
---
+++ routes/trips.js
@@ -0,0 +1,1 @@
+// const addTrip = ({ place, startDate, endDate, status, noOfTravellers }) => {};
