341f48c5958950b8810e3a32a85aa5fe024c9005
radha-rani-n <nradharani1998@gmail.com>
2025-02-11 22:04:34 -0500

    create trips table in migrations file

--- migrations/20250212023223_trips.js
+++ migrations/20250212023223_trips.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("trips", (table) => {
+    table.increments("id").primary();
+    table.string("trip_name").notNullable();
+    table.string("place_name").notNullable();
+    table.date("from_date").notNullable();
+    table.date("to_date").notNullable();
+    table.string("no_of_travellers").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+
+export function down(knex) {
+  return knex.schema.dropTable("trips");
+}
